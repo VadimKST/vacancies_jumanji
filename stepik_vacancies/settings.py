3149ef27da5d2697b6ba4c63f185f0d48e86721e
Vadim Kistanov <78316213+VadimKST@users.noreply.github.com>
2021-04-27 13:31:02 +0300

    update settings

--- stepik_vacancies/settings.py
+++ stepik_vacancies/settings.py
@@ -7,8 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'na*u^@ih^41v$h3@3gs&wrdn1!ewi6d1#$#!ciskc^ffi5)gxp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
